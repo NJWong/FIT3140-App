9eee9ad9803f6bfce5a94c844982412e751e2bfd
dashccc <dashccc@gmail.com>
2014-12-09 13:11:46 +1100

    modified

--- Assignment3/goal.py
+++ Assignment3/goal.py
@@ -3,6 +3,6 @@ from tile import Tile
 class Goal(Tile):
 	def __init__(self,posX,posY):
 		Tile.__init__(self,posX,posY)
-		self.collide = True
+		self.collide = False
 		self.win = True
 		self.desc = ' G '
