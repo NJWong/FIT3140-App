95e8f2b127e3ddac877220f876d7f426bba5814a
dashccc <dashccc@gmail.com>
2014-12-10 14:26:58 +1100

    added unit tests for main_test.py file

--- Assignment3/main_test.py
+++ Assignment3/main_test.py
@@ -1,12 +1,72 @@
 import unittest
-import main
+from main import *
 
 # testing detect_wall
+class test_detect_wall_north(unittest.TestCase):
+	def setUp(self):
+		self.test_robot = Robot(3,3,0)
+		self.test_maze = Maze()
+		self.test_main = Main(self.test_robot, self.test_maze)
 
+	def testOne(self):
+		self.assertTrue(self.test_main.detect_wall()==True)
+
+class test_detect_wall_east(unittest.TestCase):
+	def setUp(self):
+		self.test_robot = robot.Robot(2,2,1)
+		self.test_maze = maze.Maze()
+		self.test_main = main.Main(self.test_robot, self.test_maze)
+
+	def testTwo(self):
+		self.assertTrue(self.test_main.detect_wall()==True)
+
+class test_detect_wall_south(unittest.TestCase):
+	def setUp(self):
+		self.test_robot = robot.Robot(0,0,2)
+		self.test_maze = maze.Maze()
+		self.test_main = main.Main(self.test_robot, self.test_maze)
+
+	def testThree(self):
+		self.assertTrue(self.test_main.detect_wall()==True)
+
+class test_detect_wall_west(unittest.TestCase):
+	def setUp(self):
+		self.test_robot = robot.Robot(2,2,3)
+		self.test_maze = maze.Maze()
+		self.test_main = main.Main(self.test_robot, self.test_maze)
+
+	def testFour(self):
+		self.assertTrue(self.test_main.detect_wall()==True)
+
+class test_detect_wall_none(unittest.TestCase):
+	def setUp(self):
+		self.test_robot = robot.Robot(0,0,1)
+		self.test_maze = maze.Maze()
+		self.test_main = main.Main(self.test_robot, self.test_maze)
+
+	def testFive(self):
+		self.assertTrue(self.test_main.detect_wall()==False)
 
 # testing detect_win
+class test_detect_win1(unittest.TestCase):
+	def setUp(self):
+		self.test_robot = robot.Robot(3,3,0)
+		self.test_maze = maze.Maze()
+		self.test_main = main.Main(self.test_robot, self.test_maze)
 
+	def testOne(self):
+		self.assertTrue(self.test_main.detect_win()==True)
 
+class test_detect_win2(unittest.TestCase):
+	def setUp(self):
+		self.test_robot = robot.Robot(0,0,0)
+		self.test_maze = maze.Maze()
+		self.test_main = main.Main(self.test_robot, self.test_maze)
+
+	def testTwo(self):
+		self.assertTrue(self.test_main.detect_win()==False)
+
+"""
 # testing move_robot_forward
 class move_robot_forward(unittest.TestCase):
 
@@ -19,7 +79,7 @@ class move_robot_forward(unittest.TestCase):
 
 		test_main = Main(test_robot, test_maze, test_interpreter)
 
-	def testOne(self):
+	def testOne(self):"""
 
 
 # testing robot_can_move_forward
