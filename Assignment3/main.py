32a00ea05c69a93ec1dccef4d5de533cf802ad85
dashccc <dashccc@gmail.com>
2014-12-10 11:48:14 +1100

    added detect wall funtion

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -8,6 +8,26 @@ class Main:
 		self.maze = maze
 		self.interpreter = Interpreter()
 
+	def detect_wall(self):
+		"""
+		Identify the tile which the robot is facing currently is a wall or not,
+		if it is a wall, then return True; if not, return False
+		"""
+		if self.robot.direction=='N':
+			if self.maze.maze[self.robot.posX][self.robot.posY - 1].collide == True:
+				return True
+		elif self.robot.direction =='E':
+			if self.maze.maze[self.robot.posX + 1][self.robot.posY].collide == True:
+				return True		
+		elif self.robot.direction =='S':
+			if self.maze.maze[self.robot.posX][self.robot.posY + 1].collide == True:
+				return True	
+		elif self.robot.direction =='W':
+			if self.maze.maze[self.robot.posX - 1][self.robot.posY].collide == True:
+				return True		
+		else:
+			return False
+
 	def move_robot_forward(self):
 		if self.robot_can_move_forward():
 			#print('main: moving robot forward')
