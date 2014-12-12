c758472ae5c288cdf97d346c1e9f2c637c86d547
Nick Wong <njwon4@gmail.com>
2014-12-13 01:12:52 +1100

    worked out how to split NaviControls into two buttons. This is causing some problems. Committing here so that I can try something else.

--- Assignment3/navibot.py
+++ Assignment3/navibot.py
@@ -7,24 +7,41 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
 
 class NaviBot(FloatLayout):
 	#naviblocks = ObjectProperty(None)
-	#naviprogram = ObjectProperty(None)
+	naviprogram = ObjectProperty(None)
 	navicontrols = ObjectProperty(None)
 	#navimaze = ObjectProperty(None)
+
+	def run(self):
+		print('NaviBot: RUN!')
 
 class NaviBlocks(BoxLayout):
 	pass
 
 class NaviProgram(StackLayout):
-	pass
+	def run_program(self):
+		print('NaviProgram: I am running the program!')
 
 class NaviControls(BoxLayout):
-
 	run_button = ObjectProperty(None)
 	stop_button = ObjectProperty(None)
 
+	def run_button_touched(self):
+		print('NaviControls: recieved message from RunButton')
+
+	def stop_button_touched(self):
+		print('NaviControls: recieved message from StopButton')
+
+class RunButton(Button):
+	def on_press(self):
+		print('RunButton: Start the program!')
+
+class StopButton(Button):
+	def on_press(self):
+		print('StopButton: Halt program!')
 
 class NaviMaze(GridLayout):
 	pass
